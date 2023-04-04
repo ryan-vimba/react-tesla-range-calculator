ba0b83f77565047bf910c4c4ae26ffb97a995308
ryan-vimba <rvimba@gmail.com>
2023-04-04 11:13:26 -0400

    add new components to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header'
+import TeslaBattery from './containers/TeslaBattery';
+import TeslaNotice from './components/TeslaNotice/TeslaNotice';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <TeslaBattery />
+        <TeslaNotice />
       </div>
     );
   }
